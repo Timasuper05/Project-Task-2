443694dd71ae623e4266d7d7e1a83414938100ad
Timasuper05 <76902286+Timasuper05@users.noreply.github.com>
2021-01-05 00:05:28 +0300

    Initial commit

---
+++ Project Task 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Project_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            Console.WriteLine(Func(input));
+            Console.ReadLine();
+        }
+        public static int Func(string input)
+        {
+            var match = Regex.Match(input.Trim(), pattern: @"^(?<Whole>\d+)([.|,]\d*)?$");
+            if (!match.Success)
+                return 0;
+
+            var number = int.Parse(match.Groups["Whole"].Value);
+
+            if(number <= 0)
+                return 0;
+            if (number >= 100) 
+                return 100;
+
+            return (int)Math.Round((double)number / 5) * 5;
+
+        }
+    }
+}
